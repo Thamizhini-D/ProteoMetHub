ba3d98389ca932f48d680704c8f7389fe2a60dc8
Thamizhini-D <76910877+Thamizhini-D@users.noreply.github.com>
2023-06-13 23:54:42 +0200

    Add files via upload

---
+++ ProteoM2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+import pandas as pd
+import numpy as np
+import streamlit as st
+from streamlit_jupyter import StreamlitPatcher, tqdm
+
+StreamlitPatcher().jupyter()  # register streamlit with jupyter-compatible wrappers
+st.title("ProteoMetrics")
+st.subheader("Created for the Bradford Assay")
+
+
+# In[ ]:
+
+
+
+
