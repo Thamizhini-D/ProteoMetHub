2006d78988ca4d8f21439e126a1c7206856b278f
Thamizhini-D <76910877+Thamizhini-D@users.noreply.github.com>
2023-06-14 03:35:15 +0200

    Update ProteoM2.py

--- ProteoM2.py
+++ ProteoM2.py
@@ -48,19 +48,10 @@ def data_processing(data):
     data.loc[data.Standard_Unknown =='u','Protein_μg_aliquot'] = calconc(m, c, data['Average_absorbance_nm'])
             
     #drop unnecessary columns
-    data = data.drop(['Absorbance_nm', 'Replicate_number'], 1)
+    data_1 = data.drop(['Absorbance_nm', 'Replicate_number'], 1)
     
-    #drop repetetive rows
-    data.drop_duplicates(['Condition_number', 'Condition_name'], keep='first', inplace=True)
-    data.reset_index(drop=True, inplace=True)
-            
-    #calculate volume to get desired amount of proteins. Default set to 100μg
-    data.loc[data.Standard_Unknown =='u', ['Volume_(μl)_for_100μg']] = calcvol (data['Protein_μg_aliquot'], data['Aliquot_volume_μl'], 100)
-            
-    #calculate the amount of protein in entire sample based on amounts in aliquot 
-    data.loc[content_2.Standard_Unknown =='u','Protein_μg_sample'] = calcsampleconc(data['Protein_μg_aliquot'], data['Aliquot_volume_μl'], data['Sample_volume_ml'])
-    
-    return st.write(data)
+   
+    return st.write(data_1)
 
 st.title("ProteoMetrics")
 st.subheader("Created for the Bradford Assay")
