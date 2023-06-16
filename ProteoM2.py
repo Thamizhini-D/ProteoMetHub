bb03c62753b693d8155f61b9664bb35b0b85ba16
Thamizhini-D <76910877+Thamizhini-D@users.noreply.github.com>
2023-06-16 22:56:14 +0200

    Update ProteoM2.py

--- ProteoM2.py
+++ ProteoM2.py
@@ -70,7 +70,7 @@ def draw_graph(conc_x, abso_y, grad_m, inter_c):
 
            st.header("Linear Regression Model")
 
-           fig, ax = plt.subplots(figsize=(12,8))
+           fig, ax = plt.subplots(figsize=(8,4))
 
            # plot the line of best fit
            plt.plot(conc_x, abso_y, 'o')
