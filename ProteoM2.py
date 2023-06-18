7f50b10358b0a7dae9672036c9d029e262cb6565
Thamizhini-D <76910877+Thamizhini-D@users.noreply.github.com>
2023-06-18 14:45:30 +0200

    Update ProteoM2.py

--- ProteoM2.py
+++ ProteoM2.py
@@ -56,7 +56,7 @@ def draw_graph(conc_x, abso_y, grad_m, inter_c):
            plt.plot(conc_x, abso_y, 'o')
            # plot the line of best fit
            plt.plot(conc_x, grad_m*conc_x+inter_c, 'g-')
-           return st.pyplot(fig)
+           return fig
     
 def data_process_table(data, m, c):
             
@@ -119,6 +119,7 @@ if uploaded_file is not None:
         process_result = data_process_table(dataframe, m_c_output[0], m_c_output[1])  
         st.write(process_result)
         graph_result = draw_graph(conc_abso[0], conc_abso[1], m_c_output[0], m_c_output[1])
+        st.pyplot(graph_result)    
         graph_result.savefig('reg_chart')
     
             
