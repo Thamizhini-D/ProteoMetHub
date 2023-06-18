c64098120dfbcb5e9b764fee81b8edc80629a61d
Thamizhini-D <76910877+Thamizhini-D@users.noreply.github.com>
2023-06-18 13:22:47 +0200

    Update ProteoM2.py

--- ProteoM2.py
+++ ProteoM2.py
@@ -126,7 +126,6 @@ if uploaded_file is not None:
            st.divider()
            st.header("Linear Regression Model")
 
-           fig, ax = plt.subplots(figsize=(12,8))
            plt.ylabel('Absorbance at 595nm')
            plt.xlabel('Amount of proteins (μg)')
            plt.title('Graph of the standard curve')
@@ -134,6 +133,8 @@ if uploaded_file is not None:
            plt.plot(conc_abso[0], conc_abso[1], 'o')
            # plot the line of best fit
            plt.plot(conc_abso[0], m_c_output[0]*conc_abso[0]+m_c_output[1], 'g-')
+           fig, ax = plt.subplots(figsize=(12,8))
+
            st.pyplot(fig)
 
 
