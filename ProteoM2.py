fa78afab506f0f5d9e3ba78ac6f4b10ba890f36c
Thamizhini-D <76910877+Thamizhini-D@users.noreply.github.com>
2023-06-18 12:34:57 +0200

    Update ProteoM2.py

--- ProteoM2.py
+++ ProteoM2.py
@@ -122,7 +122,8 @@ if uploaded_file is not None:
         st.write(process_result)
 
         if st.button("Show graph"):
-            st.write(draw_graph(conc_abso[0], conc_abso[1], m_c_output[0], m_c_output[1]))    
+            graph = draw_graph(conc_abso[0], conc_abso[1], m_c_output[0], m_c_output[1])
+            st.write(graph)
         
         resultant_file = convert_df(process_result)
         file = plt.savefig('foo.png')    
