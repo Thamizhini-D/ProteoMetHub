1046f239c4de789255616db5e373c14d6eb1bf40
Thamizhini-D <76910877+Thamizhini-D@users.noreply.github.com>
2023-06-18 16:09:41 +0200

    Update ProteoM2.py

--- ProteoM2.py
+++ ProteoM2.py
@@ -122,12 +122,12 @@ if uploaded_file is not None:
         
         graph_result = draw_graph(conc_abso[0], conc_abso[1], m_c_output[0], m_c_output[1])
         st.pyplot(graph_result)    
-        img = io.BytesIO()
-        plt.savefig(img, format='png')
+        img = io.BytesIO(graph_result)
+        graf = plt.savefig(img, format='png')
 
         btn = st.download_button(
            label="Download image",
-           data=img,
+           data=graf,
            file_name='processed_data',
            mime="image/png"
         )
